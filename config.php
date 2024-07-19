6e861d18ea82dbb7754a9c1b4964663903f74072
Shreeja S <137512940+Shreeja-S@users.noreply.github.com>
2024-07-19 21:49:46 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$con = mysqli_connect("localhost", "root", "Elango@3355", "realestatephp");
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+?>
